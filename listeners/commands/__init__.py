78efd883d7c8222d865834f858d32607f31b0d88
bacasss <bacassga13@gmail.com>
2026-06-25 22:31:39 +0800

    Update __init__.py

--- listeners/commands/__init__.py
+++ listeners/commands/__init__.py
@@ -4,3 +4,4 @@ from .confess_command import confess_command_callback
 
 def register(app: App):
     app.command("/confess")(confess_command_callback)
+    app.command("/uselesscmd67")(confess_command_callback)
